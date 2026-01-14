2e224a4ca952eab3d5e8eb24edfe791a9ed3a5a7
dusk <dusk1@myyahoo.com>
2026-01-15 02:21:20 +0530

    added wayclick reset

--- user_scripts/update_dusky/update_dusky.sh
+++ user_scripts/update_dusky/update_dusky.sh
@@ -134,7 +134,7 @@ declare -ra UPDATE_SEQUENCE=(
 #    "U | 082_config_bat_notify.sh --default"
     "U | 083_set_thunar_terminal_kitty.sh"
     "U | 084_package_removal.sh --auto"
-#    "U | 085_wayclick_reset"
+    "U | 085_wayclick_reset"
 )
 
 # ==============================================================================
