aa58930f4b60b14f077fe5fd873ee9454906b968
dim-ghub <59436063+dim-ghub@users.noreply.github.com>
2026-02-15 23:55:44 -0600

    Delete user_scripts/vscode/setup.sh

--- user_scripts/vscode/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-cd ~
-curl -L -o material-code3.1.1.vsix https://archive.org/download/material-code3.1.1/material-code3.1.1.vsix
-vscodium --install-extension ~/material-code3.1.1.vsix
-rm ~/material-code3.1.1.vsix
-~/user_scripts/theme_matugen/theme_ctl.sh refresh
